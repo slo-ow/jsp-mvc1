8065f25cf463fc134f4ac5f90d74b1863cde9a12
slo_ow <icet25@naver.com>
2022-10-31 22:43:21 +0900

    Add MemberDAO & memberRegister.html

---
+++ src/main/java/kr/slow/model/MemberDAO.java
@@ -0,0 +1,10 @@
+package kr.slow.model;
+// JDBC -> myBatis, JPA
+import java.sql.*;
+public class MemberDAO {
+    private Connection conn;
+    private PreparedStatement ps;
+    private ResultSet rs;
+
+
+}
